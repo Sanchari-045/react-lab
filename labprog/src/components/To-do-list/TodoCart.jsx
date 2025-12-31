a4b91f6df75780ff7644dbd7c7f96fe4e2a02c52
sancharimohanty870@gmail.com <sancharimohanty870@gmail.com>
2025-12-31 12:53:12 +0530

    commit

--- labprog/src/components/To-do-list/TodoCart.jsx
+++ labprog/src/components/To-do-list/TodoCart.jsx
@@ -1,3 +1,10 @@
 export default function TodoCart() {
-    
+    return(
+        <div>
+            <h2>To-Do-List</h2>
+            <input type="text" placeholder="Add new task..." />
+            <button>Add Task</button>
+            <p>No tasks available.</p>
+        </div>
+    )
 }
