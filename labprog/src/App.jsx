40f69ef21a000d8c4712824d6e57aa3f76ef0e32
sancharimohanty870@gmail.com <sancharimohanty870@gmail.com>
2026-02-19 10:48:08 +0530

    router

--- labprog/src/App.jsx
+++ labprog/src/App.jsx
@@ -1,11 +1,18 @@
 import React from "react";
-
 import './App.css';
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Tabs from "./components/Tabs";
 import ThemeToggle from "./components/ThemeToggle";
+import ColorPicker from "./components/ColorPicker";
 function App() {
   return (
-     <ThemeToggle />
+     <BrowserRouter>
+     <Routes>
+      <Route path="/" element={<Tabs />} />
+      <Route path="/" element={<ThemeToggle />} />
+      <Route path="/" element={<ColorPicker />} />
+     </Routes>
+     </BrowserRouter>
   )
 }
 export default App
